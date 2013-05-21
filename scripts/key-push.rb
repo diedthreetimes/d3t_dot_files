de93306bd05655f6e70910c9fb63581b3096c6bd
Sky Faber <sky.faber@gmail.com>
2013-05-20 17:35:59 -0700

    Add support for multiple keys, and different home structure

--- scripts/key-push.rb
+++ scripts/key-push.rb
@@ -3,81 +3,82 @@
 require 'net/ssh'
 require 'net/scp'
 
-PRI_KEY = %w[ root.pem ]
-KEYS= %w[ ex.pub ]
-KEYS_p = %w[ ex.pem ]
+PRI_KEY = %w[ ]
 
+KEYS_TO_ADD = %w[ ]
 
 key_dir = "."
 
-HOSTS = %w[]
-user = 'ec2-user'
+HOSTS = %w[ ]
+#user = 'ec2-user'
+#home_dir = '/home/ec2-user'
+user = 'root'
+home_dir = '/root'
 
-for host in HOSTS
-  succeed = false
-  begin
+for key in KEYS_TO_ADD
+  for host in HOSTS
+    succeed = false
+    begin
+      Net::SSH.start(host, user, :keys => PRI_KEY) do |ssh|
+
+        output = ssh.exec!("whoami")
+
+        if(output =~ /#{user}/)
+          succeed = true
+        end
+      end
+
+      if !succeed then
+        puts "#{host} connection incorrect!"
+        next
+      end
+    rescue
+      puts "#{host} connection failed"
+      next
+    end
+
+    Net::SSH.start(host,user,:keys => PRI_KEY) do |ssh|
+      output = ssh.exec!("pwd")
+    end
+
+    # SSH worked now we scp over the new key
+    Net::SCP.upload!(host, user, "#{key_dir}/#{key}.pub", "#{home_dir}/.ssh/#{key}.pub", :ssh => {:keys => PRI_KEY})
+
+    # Check to see that the upload succeeded
     Net::SSH.start(host, user, :keys => PRI_KEY) do |ssh|
 
-      output = ssh.exec!("whoami")
+      output = ssh.exec!("ls #{home_dir}/.ssh/#{key}.pub")
 
-      if(output =~ /#{user}/)
-        succeed = true
+      if(!(output =~ /#{user}\/.ssh\/#{key}.pub/))
+        puts "#{host} file not found"
+        next
       end
+
+      ssh.exec!("cp #{home_dir}/.ssh/authorized_keys #{home_dir}/.ssh/authorized_keys.bak1")
+      ssh.exec!("cat #{home_dir}/.ssh/#{key}.pub >> #{home_dir}/.ssh/authorized_keys")
     end
 
-    if !succeed then
-      puts "#{host} connection incorrect!"
-      next
-    end
-  rescue
-    puts "#{host} connection failed"
-    next
-  end
+    Net::SSH.start(host, user, :keys => [key+".pem"]) do |ssh|
 
-  #TODO: loop over keys
-  key = KEYS[0]
+      output = ssh.exec!("pwd")
+      if(!(output =~ /#{user}/))
+        puts "#{host} cannot connect with new key"
+        puts output
+        next
+      end
 
-  Net::SSH.start(host,user,:keys => PRI_KEY) do |ssh|
-    output = ssh.exec!("pwd")
-  end
-
-  # SSH worked now we scp over the new key
-  Net::SCP.upload!(host, user, "#{key_dir}/#{key}", "/home/#{user}/.ssh/#{key}", :ssh => {:keys => PRI_KEY})
-
-  # Check to see that the upload succeeded
-  Net::SSH.start(host, user, :keys => PRI_KEY) do |ssh|
-
-    output = ssh.exec!("ls /home/#{user}/.ssh/#{key}")
-
-    if(!(output =~ /#{user}\/.ssh\/#{key}/))
-      puts "#{host} file not found"
-      next
     end
 
-    ssh.exec!("cp /home/#{user}/.ssh/authorized_keys /home/#{user}/.ssh/authorized_keys.bak1")
-    ssh.exec!("cat /home/#{user}/.ssh/#{key} >> /home/#{user}/.ssh/authorized_keys")
-  end
+    Net::SSH.start(host, user, :keys => PRI_KEY) do |ssh|
 
-  Net::SSH.start(host, user, :keys => KEYS_p) do |ssh|
+      output = ssh.exec!("pwd")
+      if(!(output =~ /#{user}/))
+        puts "#{host} cannot connect with old key"
+        puts output
+        next
+      end
 
-    output = ssh.exec!("pwd")
-    if(!(output =~ /#{user}/))
-      puts "#{host} cannot connect with new key"
-      puts output
-      next
     end
 
   end
-
-  Net::SSH.start(host, user, :keys => PRI_KEY) do |ssh|
-
-    output = ssh.exec!("pwd")
-    if(!(output =~ /#{user}/))
-      puts "#{host} cannot connect with old key"
-      puts output
-      next
-    end
-
-  end
-
 end
